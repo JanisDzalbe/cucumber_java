1748e3c984510c559d77a622207af2c658fc6dc8
AnastasijaGalicina <anastasijagalicina@gmail.com>
2025-05-22 21:12:41 +0300

    Task1

--- src/test/java/cucumber/stepDefinitions/SampleSteps.java
+++ src/test/java/cucumber/stepDefinitions/SampleSteps.java
@@ -4,14 +4,16 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.jupiter.api.Assertions;
+import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
 import java.util.List;
+import java.util.Locale;
 import java.util.Map;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.*;
 
 public class SampleSteps {
     private WebDriver driver;
@@ -27,6 +29,17 @@ public class SampleSteps {
     @Given("^I am on the locators page$")
     public void iAmOnTheLocatorsPage() throws Throwable {
         driver.get("https://acctabootcamp.github.io/site/examples/locators");
+    }
+    @Given("^I am on enter a number page$")
+    public void iAmOnEnterANumberPage() throws Throwable {
+        driver.get("https://acctabootcamp.github.io/site/tasks/enter_a_number");
+    }
+    @When("^I enter name \"([^\"]*)\" and age (\\d+) on feedback page$")
+    public void iEnterNameAndAgeOnFeedbackPage(String name, int age) throws Throwable {
+        driver.findElement(By.id("fb_name")).clear();
+        driver.findElement(By.id("fb_name")).sendKeys(name);
+        driver.findElement(By.id("fb_age")).clear();
+        driver.findElement(By.id("fb_age")).sendKeys(String.valueOf(age));
     }
     @Then("^I should see both locators page headers$")
     public void iShouldSeeBothLocatorsPageHeaders() throws Throwable {
@@ -58,12 +71,10 @@ public class SampleSteps {
         driver.findElement(By.id("name")).clear();
         driver.findElement(By.id("name")).sendKeys(name);
     }
-
     @And("^I enter age: (\\d+)$")
     public void iEnterAge(int age) throws Throwable {
         driver.findElement(By.id("age")).sendKeys(String.valueOf(age));
     }
-
     @Given("^I (?:am on|open) age page$")
     public void iAmOnAgePage() throws Throwable {
         driver.get("https://acctabootcamp.github.io/site/examples/age");
@@ -72,12 +83,27 @@ public class SampleSteps {
     public void iAmOnFeedbackPage() throws Throwable {
         driver.get("https://acctabootcamp.github.io/site/tasks/provide_feedback");
     }
-    @When("^I enter name \"([^\"]*)\" and age (\\d+) on feedback page$")
-    public void iEnterNameAndAgeOnFeedbackPage(String name, int age) throws Throwable {
-        driver.findElement(By.id("fb_name")).clear();
-        driver.findElement(By.id("fb_name")).sendKeys(name);
-        driver.findElement(By.id("fb_age")).clear();
-        driver.findElement(By.id("fb_age")).sendKeys(String.valueOf(age));
+    @Then("^I click submit$")
+    public void iClickSubmit() throws Throwable {
+        driver.findElement(By.className("w3-orange")).click();
+    }
+    @When("^I enter correct number: (\\d+)$")
+    public void iEnterCorrectNumber(int number) throws Throwable {
+        driver.findElement(By.id("numb")).clear();
+        driver.findElement(By.id("numb")).sendKeys(String.valueOf(number));
+    }
+    @Then("^I see the pop-up with answer for number: (\\d+)$")
+    public void iSeeThePopUpWithAnswer (String number) throws Throwable {
+        double expectedSqrt = Math.sqrt(Double.parseDouble(number));
+        String expSqrtForm = String.format(Locale.US,"%.2f", expectedSqrt);
+        Alert alert = driver.switchTo().alert();
+        String expectedAlert = "Square root of " + number + " is " + expSqrtForm;
+        Assertions.assertEquals(expectedAlert, alert.getText());
+        alert.accept();
+    }
+    @And("^No error is visible after pop-up is closed$")
+    public void noErrorIsVisibleAfterPopUpIsClosed() {
+        assertFalse(driver.findElement(By.id("ch1_error")).isDisplayed());
     }
     @And("^I click send on feedback page$")
     public void iClickSendOnFeedbackPage() throws Throwable {
@@ -145,5 +171,14 @@ public class SampleSteps {
     @Given("^I am on action page$")
     public void iAmOnActionPage() {
         driver.get("https://acctabootcamp.github.io/site/examples/actions");
+    }
+    @When("^I enter number: (.*)$")
+    public void iEnterNumberInt(String number) throws Throwable {
+        driver.findElement(By.id("numb")).clear();
+        driver.findElement(By.id("numb")).sendKeys(number);
+    }
+    @And("I see text: \"([^\"]*)\"$")
+    public void iSeeText(String error) throws Throwable {
+        assertEquals(error, driver.findElement(By.id("ch1_error")).getText());
     }
 }
--- src/test/resources/features/Task1.feature
+++ src/test/resources/features/Task1.feature
@@ -1,3 +1,26 @@
+Feature: Task1
+
+  Background:
+    Given I am on enter a number page
+
+  Scenario Outline: outline for error cases
+    When I enter number: <number>
+    Then I click submit
+    And I see text: "<error>"
+
+    Examples:
+      | number  | error                 |
+      | hello   | Please enter a number |
+      | 25      | Number is too small   |
+      | 105     |  Number is too big    |
+
+  Scenario: Correct number
+    When I enter correct number: 64
+    Then I click submit
+    Then I see the pop-up with answer for number: 64
+    And No error is visible after pop-up is closed
+
+
 # TODO - Create 1 scenario outline and 1 scenario for page with url: "https://acctabootcamp.github.io/site/tasks/enter_a_number"
 # - Scenario outline for error cases. One example for each case:
 #   * enter number too small
