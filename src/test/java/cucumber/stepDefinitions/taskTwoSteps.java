e598964d8dc4ba5011813f6f41f6b2a5ce7d65e0
Daniils Grigorjevs <daniilsgrigorjevs1@gmails.com>
2025-05-23 20:18:28 +0300

    Task 2 is done

--- src/test/java/cucumber/stepDefinitions/taskTwoSteps.java
+++ src/test/java/cucumber/stepDefinitions/taskTwoSteps.java
@@ -1,73 +1,144 @@
 package cucumber.stepDefinitions;
 
-import io.cucumber.java.en.And;
-import io.cucumber.java.en.Given;
-import io.cucumber.java.en.Then;
-import io.cucumber.java.en.When;
-import org.openqa.selenium.Alert;
+import io.cucumber.java.en.*;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
+import org.openqa.selenium.WebElement;
+import java.util.List;
 
-import java.time.Duration;
-
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 public class taskTwoSteps {
-    private WebDriver driver;
 
-    public taskTwoSteps() {
-        this.driver = Hooks.driver;
+    private WebDriver driver = Hooks.driver;
+    private final int INITIAL_COUNT = 3;
+
+    @Given("I open the people list page")
+    public void iOpenPeopleListPage() {
+        driver.get("https://acctabootcamp.github.io/site/tasks/list_of_people_with_jobs");
     }
 
-    @Given("^I am on the number entry page$")
-    public void iAmOnTheNumberEntryPage() throws Throwable {
-        driver.get("https://acctabootcamp.github.io/site/tasks/enter_a_number");
+    @And("The default list is visible")
+    public void verifyDefaultList() {
+        List<WebElement> people = driver.findElements(By.className("w3-padding-16"));
+        assertEquals(INITIAL_COUNT, people.size());
+        assertEquals("Mike", people.get(0).findElement(By.className("name")).getText());
+        assertEquals("Web Designer", people.get(0).findElement(By.className("job")).getText());
+        assertEquals("Jill", people.get(1).findElement(By.className("name")).getText());
+        assertEquals("Support", people.get(1).findElement(By.className("job")).getText());
+        assertEquals("Jane", people.get(2).findElement(By.className("name")).getText());
+        assertEquals("Accountant", people.get(2).findElement(By.className("job")).getText());
     }
 
-    @When("^I enter \"([^\"]*)\" into the number field$")
-    public void iEnterIntoNumberFields(String input) throws Throwable {
-        driver.findElement(By.id("numb")).clear();
-        driver.findElement(By.id("numb")).sendKeys("input");
+    @When("I open add person dialog")
+    public void openAddPersonDialog() {
+        driver.findElement(By.cssSelector("[onclick='openModalForAddPersonWithJob()']")).click();
     }
 
-    @When("^I enter number (\\d+) into the number field$")
-    public void iEnterNumberIntoTheNumberFieeld(int input) throws Throwable {
-        driver.findElement(By.id("numb")).clear();
-        driver.findElement(By.id("numb")).sendKeys(String.valueOf(input));
+    @And("I input {string} and {string}")
+    public void inputPersonDetails(String name, String job) {
+        WebElement nameField = driver.findElement(By.id("name"));
+        WebElement jobField = driver.findElement(By.id("job"));
+        nameField.clear();
+        nameField.sendKeys(name);
+        jobField.clear();
+        jobField.sendKeys(job);
     }
 
-
-    @And("^I click the check button$")
-    public void iClickTheCheckButton() throws Throwable {
-        driver.findElement(By.cssSelector("button[onclick='numberValidation()']")).click();
+    @And("I confirm adding the person")
+    public void confirmAddPerson() {
+        driver.findElement(By.cssSelector("[onclick='addPersonWithJobToList()']")).click();
     }
 
-    @Then("^I should see error message: \"([^\"]*)\"$")
-    public void iShouldSeeErrorMessage(String expMessage) throws Throwable {
-        String actMessage = driver.findElement(By.id("ch1_error")).getText();
-        assertEquals(expMessage, actMessage);
+    @Then("The list includes a person named {string} with job {string}")
+    public void verifyPersonAdded(String name, String job) {
+        List<WebElement> names = driver.findElements(By.className("name"));
+        List<WebElement> jobs = driver.findElements(By.className("job"));
+        assertEquals(name, names.get(names.size() - 1).getText());
+        assertEquals(job, jobs.get(jobs.size() - 1).getText());
     }
 
-    @Then("^I should see a success alert with the correct square root of (\\d+)")
-    public void iShouldSeeASuccessAllertWithTheCorrectSquareRoot(int number) {
-
-
-        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(5));
-        Alert alert = wait.until(ExpectedConditions.alertIsPresent());
-
-        double expectedRoot = Math.round(Math.sqrt(number) * 100.0) / 100.0;
-        String expectedText = "Square root of " + number + " is " + expectedRoot;
-
-        String actualAlertText = alert.getText();
-        assertEquals(expectedText, actualAlertText);
-
-        alert.accept();
+    @When("I edit the {int}{word} person")
+    public void selectPersonToEdit(int index, String ordinal) {
+        driver.findElements(By.className("w3-padding-16")).get(index - 1).findElement(By.className("editbtn")).click();
     }
 
+    @And("I update the job field to {string}")
+    public void updateJob(String job) {
+        WebElement jobInput = driver.findElement(By.id("job"));
+        jobInput.clear();
+        jobInput.sendKeys(job);
+    }
 
+    @And("I update the name field to {string}")
+    public void updateName(String name) {
+        WebElement nameInput = driver.findElement(By.id("name"));
+        nameInput.clear();
+        nameInput.sendKeys(name);
+    }
 
+    @And("I update the name to {string} and job to {string}")
+    public void updateNameAndJob(String name, String job) {
+        updateName(name);
+        updateJob(job);
+    }
 
+    @And("Press edit Button")
+    public void saveChanges() {
+        driver.findElement(By.xpath("//*[@id='modal_button' and text()='Edit']")).click();
+    }
 
+    @Then("The {int}{word} person’s name is {string} and job is {string}")
+    public void verifyNameAndJobUpdated(int index, String ordinal, String name, String job) {
+        WebElement person = driver.findElements(By.className("w3-padding-16")).get(index - 1);
+        assertEquals(name, person.findElement(By.className("name")).getText());
+        assertEquals(job, person.findElement(By.className("job")).getText());
+    }
+
+    @When("I delete the person name {string}")
+    public void deletePersonByName(String name) {
+        List<WebElement> people = driver.findElements(By.className("w3-padding-16"));
+        for (WebElement person : people) {
+            if (person.findElement(By.className("name")).getText().equals(name)) {
+                person.findElement(By.className("closebtn")).click();
+                break;
+            }
+        }
+    }
+
+    @Then("The list does not contain {string}")
+    public void verifyPersonDeleted(String name) {
+        List<WebElement> people = driver.findElements(By.className("w3-padding-16"));
+        for (WebElement person : people) {
+            assertNotEquals(name, person.findElement(By.className("name")).getText());
+        }
+    }
+
+    @And("The total number of people decreased")
+    public void verifyListIsShorter() {
+        assertNotEquals(INITIAL_COUNT, driver.findElements(By.className("w3-padding-16")).size());
+    }
+
+    @When("I reset the people list")
+    public void resetPeopleList() {
+        driver.findElement(By.cssSelector("[onclick='resetListOfPeople()']")).click();
+    }
+
+    @Then("The default list is shown")
+    public void verifyDefaultListIsShown() {
+        verifyDefaultList();
+    }
+
+    @And("I clear all inputs")
+    public void clearInputFields() {
+        driver.findElement(By.id("addPersonBtn")).click();
+    }
+
+    @Then("All input fields are empty")
+    public void verifyInputsAreEmpty() {
+        WebElement nameField = driver.findElement(By.id("name"));
+        WebElement jobField = driver.findElement(By.id("job"));
+        assertEquals("", nameField.getAttribute("value"));
+        assertEquals("", jobField.getAttribute("value"));
+    }
 }
--- src/test/resources/features/Task2.feature
+++ src/test/resources/features/Task2.feature
@@ -10,3 +10,61 @@
 #   * editing a person
 #   * removing a person
 # - check that clear button on adding a user works correctly
+Feature: People List Management
+
+  Background:
+    Given I open the people list page
+    And The default list is visible
+
+  Scenario Outline: Add new person
+    When I open add person dialog
+    And I input "<Name>" and "<Job>"
+    And I confirm adding the person
+    Then The list includes a person named "<Name>" with job "<Job>"
+
+    Examples:
+      |Name       |Job          |
+      |Alice      |Developer    |
+      |Daniils    |Programmer   |
+      |Mark       |Web Developer|
+
+
+  Scenario: Edit a Person
+    When I edit the 3rd person
+    And I update the name to "Inga" and job to "Teacher"
+    And Press edit Button
+    Then The 3rd person’s name is "Inga" and job is "Teacher"
+
+  Scenario: Delete a person
+    When I delete the person name "Mike"
+    Then The list does not contain "Mike"
+    And The total number of people decreased
+
+  Scenario: Reset list after adding a person
+    When I open add person dialog
+    And I input "Marcis" and "Programmer"
+    And I confirm adding the person
+    Then The list includes a person named "Marcis" with job "Programmer"
+    When I reset the people list
+    Then The default list is shown
+
+  Scenario: Reset list after editing a person
+    When I edit the 1st person
+    And I update the name to "Inga" and job to "Teacher"
+    And Press edit Button
+    Then The 1st person’s name is "Inga" and job is "Teacher"
+    When I reset the people list
+    Then The default list is shown
+
+  Scenario: Reset list after deleting a person
+    When I delete the person name "Mike"
+    Then The list does not contain "Mike"
+    And The total number of people decreased
+    When I reset the people list
+    Then The default list is shown
+
+  Scenario: Clear input fields on add dialog
+    When I open add person dialog
+    And I input "Test" and "Text"
+    And I clear all inputs
+    Then All input fields are empty
