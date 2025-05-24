26af03847e5eb10d42682930be5d6579b0ed3d7d
Santana S <santana.voinikevica@gmail.com>
2025-05-24 18:19:39 +0300

    Class work

--- src/test/java/cucumber/stepDefinitions/SampleStepsHomework.java
+++ src/test/java/cucumber/stepDefinitions/SampleStepsHomework.java
@@ -4,6 +4,7 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.junit.Before;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
@@ -11,8 +12,8 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import java.util.List;
 import java.util.Map;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.Assert.*;
 
 public class SampleStepsHomework {
     private WebDriver driver;
@@ -33,9 +34,14 @@ public class SampleStepsHomework {
         driver.get("https://acctabootcamp.github.io/site/tasks/list_of_people_with_jobs.html");
     }
 
+    @When("^I count the number of records on the page$")
+    public void iCountTheNumberOfTheRecords() throws Throwable {
+        initialRecordCount = getInitialRecordCount();
+    }
+
     @When("^I click on the \"Add person\" button$")
     public void iClickOnAddPerson() throws Throwable {
-        driver.findElement(By.id("addPersonBtn")).click();
+        driver.findElement(By.xpath("//button[normalize-space(text())='Add person']")).click();
     }
 
     @Then("I am redirected to the Add new person page$")
@@ -63,34 +69,90 @@ public class SampleStepsHomework {
 
     @And("^I can see that the number of records has increased$")
     public void seeRecordNumberHasIncreaseOnMainPage() throws Throwable {
-        initialRecordCount = getInitialRecordCount();
+        initialRecordCount = initialRecordCount + 1;
         List<WebElement> recordNum = driver.findElements(By.className("w3-xlarge"));
         Assertions.assertEquals(initialRecordCount, recordNum.size());
-        }
-    @When("^I choose the second record and click on the pencil icon to edit it$")
-    public void iClickOnPencilIcon() throws Throwable {
-        driver.findElement(By.cssSelector("#person1 i.fa-pencil")).click();
-    }
-    @And("I can see that the name and job have been updated to \"Simon\" and \"Florist\"$")
-    public void checkThatNameAndJobHasBeenChanged() throws Throwable {
-        assertEquals("Simon", driver.findElement(By.cssSelector("#person1 > span.w3-xlarge")).getText());
-        System.out.println(driver.findElement(By.cssSelector("#person0 > span.w3-xlarge")).getText());
     }
 
-    @Then("I am redirected to the Editing page$")
-    public void redirectionToTheEditingPage() throws Throwable {
+    @When("^I choose the second record and click on the pencil icon to edit it$")
+    public void iClickOnPencilIcon1() throws Throwable {
+        driver.findElement(By.cssSelector("#person1 i.fa-pencil")).click();
+    }
+
+    @And("I can see that the name and job have been updated to name \"([^\"]*)\" and job \"([^\"]*)\"$")
+    public void checkThatNameAndJobHasBeenChanged(String name, String job) throws Throwable {
+        assertEquals(name, driver.findElement(By.cssSelector("#person1 > span.w3-xlarge")).getText());
+        assertEquals(job, driver.findElement(By.cssSelector("#person1 > span.job")).getText());
+        }
+
+    @Then("I am redirected to the Editing page for second record$")
+    public void redirectionToTheEditingPage1() throws Throwable {
         assertEquals("https://acctabootcamp.github.io/site/tasks/enter_a_new_person_with_a_job.html?id=1", driver.getCurrentUrl());
-}
+    }
+
     @And("^I click on the \"Edit\" button$")
     public void iClickOnEditButtonAtEditPersonPage() throws Throwable {
         driver.findElement(By.xpath("(//button[normalize-space(text())='Edit'])[1]")).click();
     }
 
-    @When("^I click on the \"x\" icon next to the record to delete it$"){
-    public void iClickOnDeleteButtonAtPersonPage() throws Throwable {
+    @When("^I click on the \"x\" icon next to the third record to delete it$")
+    public void iClickOnDeleteButtonAtPersonPage2() throws Throwable {
         driver.findElement(By.cssSelector("#person2 > span.closebtn")).click();
     }
+
+    @Then("I remain on the main page$")
+    public void remaneOnTheMainPage() throws Throwable {
+        assertEquals("https://acctabootcamp.github.io/site/tasks/list_of_people_with_jobs.html", driver.getCurrentUrl());
+    }
+
+    @And("^I can see that the number of records has decreased$")
+    public void seeRecordNumberHasDecreaseOnMainPage() throws Throwable {
+        initialRecordCount = initialRecordCount - 1;
+        List<WebElement> recordNum = driver.findElements(By.className("w3-xlarge"));
+        Assertions.assertEquals(initialRecordCount, recordNum.size());
+    }
+    @When("^I click on the \"Reset List\" button$")
+    public void iClickOnResetListOnAddNewPersonPage() throws Throwable {
+        driver.findElement(By.xpath("//button[normalize-space(text())='Reset List']")).click();
+    }
+    @When("^I choose the third record and click on the pencil icon to edit it$")
+    public void iClickOnPencilIcon2() throws Throwable {
+        driver.findElement(By.cssSelector("#person2 i.fa-pencil")).click();
+    }
+    @Then("I am redirected to the Editing page for third record$")
+    public void redirectionToTheEditingPage2() throws Throwable {
+        assertEquals("https://acctabootcamp.github.io/site/tasks/enter_a_new_person_with_a_job.html?id=2", driver.getCurrentUrl());
+    }
+    @Then("I can see changes for name \"([^\"]*)\" and job \"([^\"]*)\" are not saved$")
+    public void checkThatNameAndJobHasNotBeenChanged2(String name, String job) throws Throwable {
+        assertNotEquals(name, driver.findElement(By.cssSelector("#person2 > span.w3-xlarge")).getText());
+        assertNotEquals(job, driver.findElement(By.cssSelector("#person2 > span.job")).getText());
+    }
+    @When("^I click on the \"x\" icon next to the first record to delete it$")
+    public void iClickOnDeleteButtonAtPersonPage0() throws Throwable {
+        driver.findElement(By.cssSelector("#person0 > span.closebtn")).click();
+    }
+    @When("^I clear name and job fields$")
+    public void iEnterNameAndJob() throws Throwable {
+        driver.findElement(By.id("name")).clear();
+        driver.findElement(By.id("job")).clear();
+    }
+    @When("^I click on the \"Clear all fields\" button$")
+    public void iClickOnClearAllFields() throws Throwable {
+        driver.findElement(By.cssSelector("#addPersonBtn")).click();
+    }
+    @Then("^I see empty Name and Job fields$")
+    public void iSeeEmptyNameAndJobFieldsEnterNameAndJob() throws Throwable {
+        assertEquals("", driver.findElement(By.id("name")).getText());
+        assertEquals("", driver.findElement(By.id("job")).getText());
+    }
+    @And("I can see that the name and job fields of fourth record are empty$")
+    public void checkThatNameAndJobFieldsAreEmpty3() throws Throwable {
+        assertEquals("", driver.findElement(By.cssSelector("#person3 > span.w3-xlarge")).getText());
+        assertEquals("", driver.findElement(By.cssSelector("#person3 > span.job")).getText());
+    }
 }
+
 
 
 
--- src/test/resources/features/Task2.feature
+++ src/test/resources/features/Task2.feature
@@ -19,7 +19,8 @@ Feature: Introduction to cucumber part 3: Homework, Task 2
 #Test 1
 Scenario Outline: User can add a new person
 
-When I click on the "Add person" button
+When I count the number of records on the page
+And I click on the "Add person" button
 Then I am redirected to the Add new person page
 When I enter a new name "<name>" and a new job "<job>"
 And I click on the "Add" button
@@ -34,57 +35,60 @@ And I can see that the number of records has increased
 #Test 2
 Scenario: User can edit a person
   When I choose the second record and click on the pencil icon to edit it
-  Then I am redirected to the Editing page
+  Then I am redirected to the Editing page for second record
   When I enter a new name "Simon" and a new job "Florist"
   And I click on the "Edit" button
   Then I am redirected to the main page
-  And I can see that the name and job have been updated to "Simon" and "Florist"
+  And I can see that the name and job have been updated to name "Simon" and job "Florist"
 
 #Test 3
-Scenario  User can remove a person
+Scenario:  User can remove a person
 
-  When I click on the "x" icon next to the third record to delete it
+  When I count the number of records on the page
+  And I click on the "x" icon next to the third record to delete it
   Then I remain on the main page
   And I can see that the number of records has decreased
 
-##Test 4
-#Scenario: User resets main page after adding a new person
-#
-#  When I click on the "Add person" button
-#  Then I am redirected to the Editing page
-#  When I enter a new name "Alise" and a new job "Dancer"
-#  And I click on the "Add" button
-#  Then I am redirected to the main page
-#  And I can see that the number of records has increased
-#  When I click on the "Reset List" button
-#  Then I can see the record number has returned to the previous value
-#
-#
-##Test 5
-#Scenario: User resets main page after editing a person
-#  When I choose a record and click on the pencil icon to edit it
-#  Then I am redirected to the Add new person page
-#  When I enter a new name "Simon" and a new job "Florist"
-#  And I click on the "Edit" button
-#  Then I am redirected to the main page
-#  When I click on the "Reset List" button
-#  Then I can see changes for name "Simon" and job "Florist" are not saved
-#
-# #Test 6
-#Scenario: User resets main page after removing a person
-#  When I click on the "x" icon next to the record to delete it
-#  Then I remain on the main page
-#  And I can see that the number of records has decreased
-#  When I click on the "Reset List" button
-#  Then I can see the record number has returned to the previous value
-#
-##Test 7
-#Scenario: User checks that clear button on adding a person works correctly
-#  When I click on the "Add person" button
-#  Then I am redirected to the Add new person page
-#  When I clear name and job fields
-#  And I click on the "Add" button
-#  Then I am redirected to the main page
-#  And I can see that the number of records has increased
-#  When I click on the "Reset List" button
-#  Then I can see that the name and job fields of record are emphty
+#Test 4
+Scenario: User resets main page after adding a new person
+
+  When I count the number of records on the page
+  And I click on the "Add person" button
+  Then I am redirected to the Add new person page
+  When I enter a new name "Alise" and a new job "Dancer"
+  And I click on the "Add" button
+  Then I am redirected to the main page
+  And I can see that the number of records has increased
+  When I click on the "Reset List" button
+  Then I can see that the number of records has decreased
+
+
+#Test 5
+Scenario: User resets main page after editing a person
+  When I choose the third record and click on the pencil icon to edit it
+  Then I am redirected to the Editing page for third record
+  When I enter a new name "Damir" and a new job "Sportist"
+  And I click on the "Edit" button
+  Then I am redirected to the main page
+  When I click on the "Reset List" button
+  Then I can see changes for name "Damir" and job "Sportist" are not saved
+
+ #Test 6
+Scenario: User resets main page after removing a person
+  When I count the number of records on the page
+  And I click on the "x" icon next to the first record to delete it
+  Then I remain on the main page
+  And I can see that the number of records has decreased
+  When I click on the "Reset List" button
+  Then I can see that the number of records has increased
+
+#Test 7
+Scenario: User checks that clear button on adding a person works correctly
+  When I click on the "Add person" button
+  Then I am redirected to the Add new person page
+  When I enter a new name "Pavel" and a new job "Programmer"
+  And I click on the "Clear all fields" button
+  Then I see empty Name and Job fields
+  When I click on the "Add" button
+  Then I am redirected to the main page
+  And I can see that the name and job fields of fourth record are empty
