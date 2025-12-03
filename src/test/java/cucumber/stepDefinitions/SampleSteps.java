8c1749d9f62ac2ecf68da62fb596cd85f1b78895
RoboStriker2k <rlaimite@gmail.com>
2025-12-03 10:48:31 +0200

    Implemented Sample1 and sample2 feature test

--- src/test/java/cucumber/stepDefinitions/SampleSteps.java
+++ src/test/java/cucumber/stepDefinitions/SampleSteps.java
@@ -1,5 +1,6 @@
 package cucumber.stepDefinitions;
 
+import io.cucumber.java.PendingException;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -12,91 +13,127 @@ import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertAll;
 
 public class SampleSteps {
-    private WebDriver driver;
+  private WebDriver driver;
 
-    public SampleSteps() {
-        this.driver = Hooks.driver;
-    }
+  public SampleSteps() {
+    this.driver = Hooks.driver;
+  }
 
-    @Given("^I am on the home page$")
-    public void iAmOnTheHomePage() throws Throwable {
-        driver.get("https://acctabootcamp.github.io/site");
-    }
+  @Given("^I am on the home page$")
+  public void iAmOnTheHomePage() throws Throwable {
+    driver.get("https://janisdzalbe.github.io/example-site/");
+  }
 
-    @Then("^I should see home page header$")
-    public void iShouldSeeHomePageHeader() throws Throwable {
-        assertEquals("This is a home page",
-                driver.findElement(By.cssSelector("h1")).getText());
-    }
+  @Then("^I should see home page header$")
+  public void iShouldSeeHomePageHeader() throws Throwable {
+    assertEquals("This is a home page",
+            driver.findElement(By.cssSelector("h1")).getText());
+  }
 
-    @And("^I should see home page description$")
-    public void iShouldSeeHomePageDescription() throws Throwable {
-        assertEquals("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
-                driver.findElement(By.cssSelector("p")).getText());
-    }
+  @And("^I should see home page description$")
+  public void iShouldSeeHomePageDescription() throws Throwable {
+    assertEquals("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.",
+            driver.findElement(By.cssSelector("p")).getText());
+  }
 
-    @When("^I enter name: \"([^\"]*)\"$")
-    public void iEnterName(String name) throws Throwable {
-        driver.findElement(By.id("name")).clear();
-        driver.findElement(By.id("name")).sendKeys(name);
-    }
+  @When("^I enter name: \"([^\"]*)\"$")
+  public void iEnterName(String name) throws Throwable {
+    driver.findElement(By.id("name")).clear();
+    driver.findElement(By.id("name")).sendKeys(name);
+  }
 
-    @And("^I enter age: (\\d+)$")
-    public void iEnterAge(int age) throws Throwable {
-        driver.findElement(By.id("age")).sendKeys(String.valueOf(age));
-    }
+  @And("^I enter age: (\\d+)$")
+  public void iEnterAge(int age) throws Throwable {
+    driver.findElement(By.id("age")).sendKeys(String.valueOf(age));
+  }
 
-    @Given("^I (?:am on|open) age page$")
-    public void iAmOnAgePage() throws Throwable {
-        driver.get("https://janisdzalbe.github.io/example-site/examples/age");
-    }
+  @Given("^I (?:am on|open) age page$")
+  public void iAmOnAgePage() throws Throwable {
+    driver.get("https://janisdzalbe.github.io/example-site/examples/age");
+  }
 
-    @And("^I click submit age$")
-    public void iClickSubmitAge() throws Throwable {
-        driver.findElement(By.id("submit")).click();
-    }
+  @And("^I click submit age$")
+  public void iClickSubmitAge() throws Throwable {
+    driver.findElement(By.id("submit")).click();
+  }
 
-    @Then("^I see message: \"([^\"]*)\"$")
-    public void iSeeMessage(String message) throws Throwable {
-        assertEquals(message, driver.findElement(By.id("message")).getText());
-    }
+  @Then("^I see message: \"([^\"]*)\"$")
+  public void iSeeMessage(String message) throws Throwable {
+    assertEquals(message, driver.findElement(By.id("message")).getText());
+  }
 
-    @When("^I enter values:$")
-    public void iEnterValues(Map<String, String> valuesToEnter) throws Throwable {
-        for (Map.Entry<String, String> e : valuesToEnter.entrySet()) {
-            driver.findElement(By.id(e.getKey())).clear();
-            driver.findElement(By.id(e.getKey())).sendKeys(e.getValue());
-            System.out.println("key is " + e.getKey());
-            System.out.println("value is " + e.getValue());
-        }
+  @When("^I enter values:$")
+  public void iEnterValues(Map<String, String> valuesToEnter) throws Throwable {
+    for (Map.Entry<String, String> e : valuesToEnter.entrySet()) {
+      driver.findElement(By.id(e.getKey())).clear();
+      driver.findElement(By.id(e.getKey())).sendKeys(e.getValue());
+      System.out.println("key is " + e.getKey());
+      System.out.println("value is " + e.getValue());
     }
+  }
 
-    @And("^I should see menu$")
-    public void iShouldSeeMenu() throws Throwable {
-        assertTrue(driver.findElement(By.className("w3-navbar")).isDisplayed());
-    }
+  @And("^I should see menu$")
+  public void iShouldSeeMenu() throws Throwable {
+    assertTrue(driver.findElement(By.className("w3-navbar")).isDisplayed());
+  }
 
-    @And("^I click the result checkbox button$")
-    public void iClickTheResultCheckboxButton() throws Throwable {
-        driver.findElement(By.id("result_button_checkbox")).click();
-    }
+  @And("^I click the result checkbox button$")
+  public void iClickTheResultCheckboxButton() throws Throwable {
+    driver.findElement(By.id("result_button_checkbox")).click();
+  }
 
-    @When("^I clicked on checkboxes:$")
-    public void iClickedOnCheckboxes(List<String> values) throws Throwable {
-        for (String value : values) {
-            driver.findElement(By.cssSelector("[value='" + value + "']")).click();
-        }
+  @When("^I clicked on checkboxes:$")
+  public void iClickedOnCheckboxes(List<String> values) throws Throwable {
+    for (String value : values) {
+      driver.findElement(By.cssSelector("[value='" + value + "']")).click();
     }
+  }
 
-    @Then("^message for checkboxes \"([^\"]*)\" is seen$")
-    public void messageForCheckboxesIsSeen(String message) throws Throwable {
-        assertEquals(message, driver.findElement(By.id("result_checkbox")).getText());
-    }
+  @Then("^message for checkboxes \"([^\"]*)\" is seen$")
+  public void messageForCheckboxesIsSeen(String message) throws Throwable {
+    assertEquals(message, driver.findElement(By.id("result_checkbox")).getText());
+  }
 
-    @Given("^I am on action page$")
-    public void iAmOnActionPage() {
-        driver.get("https://janisdzalbe.github.io/example-site/examples/actions");
-    }
+  @Given("^I am on action page$")
+  public void iAmOnActionPage() {
+    driver.get("https://janisdzalbe.github.io/example-site/examples/actions");
+  }
+
+  @When("I am on the locators page")
+  public void iAmOnTheLocatorsPage() {
+
+    driver.get("https://janisdzalbe.github.io/example-site/examples/locators");
+
+  }
+
+  @Then("I should see both locators page headers")
+  public void iShouldSeeBothLocatorsPageHeaders() {
+
+    assertTrue(driver.findElement(By.id("heading_1")).isDisplayed());
+  }
+
+  @And("Buttons in Locators page are clickable")
+  public void buttonsInLocatorsPageAreClickable() {
+    assertAll(
+            ()->assertTrue( driver.findElement(By.name("randomButton1")).isEnabled()),
+            ()->assertTrue( driver.findElement(By.name("randomButton1")).isDisplayed()),
+            ()->assertTrue( driver.findElement(By.name("randomButton2")).isEnabled()),
+            ()->assertTrue( driver.findElement(By.name("randomButton2")).isDisplayed())
+    );
+
+  }
+
+  @Then("I see error: {string}")
+  public void iSeeError(String arg0) {
+      assertEquals(arg0,driver.findElement(By.id("error")).getText());
+  }
+
+  @And("I am not navigated to age message page")
+  public void iAmNotNavigatedToAgeMessagePage() {
+      assertEquals("https://janisdzalbe.github.io/example-site/examples/age",driver.getCurrentUrl());
+
+  }
 }
