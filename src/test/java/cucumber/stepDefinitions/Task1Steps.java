c03434b960c321d17f9dd46158d8acf7d4b478ac
Mariss Zakaritis <marissz@proton.me>
2025-12-03 15:04:43 +0200

    Completed Task1

---
+++ src/test/java/cucumber/stepDefinitions/Task1Steps.java
@@ -0,0 +1,64 @@
+package cucumber.stepDefinitions;
+
+import com.google.common.base.Equivalence;
+import io.cucumber.java.PendingException;
+import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import org.openqa.selenium.Alert;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.junit.Assert.*;
+
+
+public class Task1Steps {
+    private WebDriver driver;
+
+    public Task1Steps() {
+        this.driver = Hooks.driver;
+    }
+
+    @Given("^I am on Enter a number page$")
+    public void iAmOnEnterANumberPage() {
+        driver.get("https://janisdzalbe.github.io/example-site/tasks/enter_a_number");
+    }
+
+    @When("^I enter \"([^\"]*)\" in number field$")
+    public void iEnterInNumberField(String input) {
+        WebElement inputField = driver.findElement(By.id("numb"));
+        inputField.clear();
+        inputField.sendKeys(input);
+    }
+
+    @When("^I press Submit button$")
+    public void iPressSubmitButton() {
+        driver.findElement(By.tagName("button")).click();
+    }
+
+    @Then("^I see the message: \"([^\"]*)\"$")
+    public void iSeeTheMessage(String message) {
+        assertEquals(message, driver.findElement(By.id("ch1_error")).getText());
+    }
+
+    @Then("^I confirm the alert shows the square root for \"([^\"]*)\"$")
+    public void iConfirmTheAlertShowsTheSquareRootForInput(String input) {
+        Alert alert = driver.switchTo().alert();
+        double sqrt = Math.sqrt(Double.parseDouble(input));
+        System.out.printf("Calculated: %s, Rounded: %.2f", sqrt, sqrt);
+
+        String alertAssertion = String.format("Square root of %s is %.2f", input, sqrt);
+        assertEquals(alertAssertion, alert.getText());
+    }
+
+    @And("^I check no error message is shown after closing the alert$")
+    public void iCheckNoErrorMessageIsShownAfterClosingTheAlert() {
+        driver.switchTo().alert().accept();
+        assertFalse(driver.findElement(By.id("ch1_error")).isDisplayed());
+    }
+}
--- src/test/resources/features/Task1.feature
+++ src/test/resources/features/Task1.feature
@@ -4,3 +4,32 @@
 #   * enter number too big
 #   * enter text instead of the number
 # - Scenario for correct number
+
+Feature: Task 1
+  As a test engineer
+  I want to be able to write and execute a scenario with parameters
+
+  Background:
+    Given I am on Enter a number page
+
+  Scenario Outline: handling error cases
+    When I enter "<input>" in number field
+    And I press Submit button
+    Then I see the message: "<message>"
+
+    Examples:
+      | input | message               |
+      | 49    | Number is too small   |
+      | 101   | Number is too big     |
+      | aaa   | Please enter a number |
+
+  Scenario Outline: entering correct number
+    When I enter "<input>" in number field
+    And I press Submit button
+    Then I confirm the alert shows the square root for "<input>"
+    And I check no error message is shown after closing the alert
+
+    Examples:
+      | input |
+      | 51    |
+      | 99    |
