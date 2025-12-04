2f81857395d6193cba126ced582d3390422a246e
beate_scerbane <bscerbane1@gmail.com>
2025-12-04 11:49:45 +0200

    SampleUsingPO2.feature

--- src/test/java/cucumber/stepDefinitions/SamplePOSteps.java
+++ src/test/java/cucumber/stepDefinitions/SamplePOSteps.java
@@ -4,9 +4,12 @@ import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.PageFactory;
 import cucumber.pages_sample.*;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 import java.util.Map;
 
@@ -36,6 +39,7 @@ public class SamplePOSteps {
     @Given("^I (?:am on|open) age page using PO$")
     public void iAmOnAgePage() throws Throwable {
         driver.get(agePage.getPageUrl());
+        //Thread.sleep(3000);
     }
 
     @And("^I click submit age using PO$")
@@ -52,5 +56,26 @@ public class SamplePOSteps {
     public void iEnterValues(Map<String, String> valuesToEnter) throws Throwable {
         agePage.enterName(valuesToEnter.get("name"));
         agePage.enterAge(valuesToEnter.get("age"));
+    }
+
+        //SampleUsingPO2
+
+
+    @Then("^I see error: \"([^\"]*)\" using PO$")
+    public void iSeeErrorUsingPO(String errorMessage) throws Throwable {
+        assertEquals(errorMessage, driver.findElement(By.id("error")).getText());
+    }
+
+    @And("^I remain in age page using PO$")
+    public void iRemainInAgePageUsingPO() throws Throwable {
+        String currentUrl = driver.getCurrentUrl();
+        assertTrue(
+                "We should remain on the age page",
+                currentUrl.endsWith("/examples/age")
+        );
+        assertFalse(                //Assure we are not on /message/result page
+                "URL should not contain query parameters indicating navigation",
+                currentUrl.contains("?") || currentUrl.contains("message=")
+        );
     }
 }
