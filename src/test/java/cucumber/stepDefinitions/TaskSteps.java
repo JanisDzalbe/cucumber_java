1650121bdfd92cec1a2293d3cd082bbbce65893a
tuttifrutti7 <daces777@gmail.com>
2025-12-04 14:36:46 +0200

    Completed Task 2

--- src/test/java/cucumber/stepDefinitions/TaskSteps.java
+++ src/test/java/cucumber/stepDefinitions/TaskSteps.java
@@ -1,5 +1,6 @@
 package cucumber.stepDefinitions;
 
+import cucumber.customDataTableType.PersonData;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -7,6 +8,7 @@ import io.cucumber.java.en.When;
 import org.openqa.selenium.Alert;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 import java.util.List;
 import java.util.Map;
@@ -56,4 +58,118 @@ public class TaskSteps {
         assertFalse(driver.findElement(By.id("ch1_error")).isDisplayed());
     }
 
+
+    // Task 2
+    @Given("^I am on the list of people with jobs page$")
+    public void iAmOnTheListOfPeopleWithJobsPage() throws Throwable {
+        driver.get("https://janisdzalbe.github.io/example-site/tasks/list_of_people_with_jobs.html");
+    }
+
+    @When("^I click add person$")
+    public void iClickAddPerson() throws Throwable {
+        driver.findElement(By.xpath("//button[text()='Add person']")).click();
+    }
+
+    @When("^I fill fields$")
+    public void iFillFields(Map<String, String> valuesToEnter) throws Throwable {
+        for (Map.Entry<String, String> entry : valuesToEnter.entrySet()) {
+            driver.findElement(By.id(entry.getKey())).clear();
+            driver.findElement(By.id(entry.getKey())).sendKeys(entry.getValue());
+        }
+    }
+
+    @When("^I click Add button$")
+    public void iClickAddButton() throws Throwable {
+        driver.findElement(By.xpath("//button[text()='Add']")).click();
+    }
+
+    @Then("^I see person added to the list$")
+    public void iSeePersonAddedToTheList(Map<String, String> valuesToCheck) throws Throwable {
+        List<WebElement> people = driver.findElements(By.className("w3-padding-16"));
+        for (WebElement person : people) {
+            if (person.findElement(By.className("name")).getText().equals(valuesToCheck.get("name"))) {
+                assertEquals(valuesToCheck.get("job"), person.findElement(By.className("job")).getText());
+            }
+        }
+    }
+
+    @When("^I click Edit button for person: \"([^\"]*)\"$")
+    public void iClickEditButtonForPerson(String personName) throws Throwable {
+        List<WebElement> people = driver.findElements(By.className("w3-padding-16"));
+        for (WebElement person : people) {
+            if (person.findElement(By.className("name")).getText().equals(personName)) {
+                person.findElement(By.className("fa-pencil")).click();
+                return;
+            }
+        }
+    }
+
+    @When("^I see person values$")
+    public void iSeePersonValues(Map<String, String> valuesToCheck) throws Throwable {
+        assertEquals(valuesToCheck.get("name"), driver.findElement(By.id("name")).getAttribute("value"));
+        assertEquals(valuesToCheck.get("job"), driver.findElement(By.id("job")).getAttribute("value"));
+    }
+
+    @When("^I change Job: \"([^\"]*)\"$")
+    public void iChangeJob(String job) throws Throwable {
+        driver.findElement(By.id("job")).clear();
+        driver.findElement(By.id("job")).sendKeys(job);
+    }
+
+    @When("^I click Edit button$")
+    public void iClickEditButton() throws Throwable {
+        driver.findElement(By.xpath("//button[text()='Edit']")).click();
+    }
+
+    @Then("^I see person updated$")
+    public  void iSeePersonUpdated(Map<String, String> valuesToCheck) throws Throwable {
+        List<WebElement> people = driver.findElements(By.className("w3-padding-16"));
+        for (WebElement person : people) {
+            if (person.findElement(By.className("name")).getText().equals(valuesToCheck.get("name"))) {
+                assertEquals(valuesToCheck.get("job"), person.findElement(By.className("job")).getText());
+                return;
+            }
+        }
+    }
+
+    @When("^I click Delete button for person: \"([^\"]*)\"$")
+    public void iClickDeleteButtonForPerson(String personName) throws Throwable {
+        List<WebElement> people = driver.findElements(By.className("w3-padding-16"));
+        for (WebElement person : people) {
+            if (person.findElement(By.className("name")).getText().equals(personName)) {
+                person.findElement(By.className("closebtn")).click();
+                return;
+            }
+        }
+    }
+
+    @Then("^I see person \"([^\"]*)\" is removed from the list$")
+    public void iSeePersonIsRemovedFromTheList(String personName) throws Throwable {
+        List<WebElement> people = driver.findElements(By.className("w3-padding-16"));
+        for (WebElement person : people) {
+            if (person.findElement(By.className("name")).getText().equals(personName)) {
+                fail();
+                return;
+            }
+        }
+    }
+
+    @When("^click Reset List$")
+    public void clickResetList() throws Throwable {
+        driver.findElement(By.xpath("//button[text()='Reset List']")).click();
+    }
+
+    @Then("^I see list is back to initial state$")
+    public void iSeeListIsBackToInitialState() throws Throwable {
+        String[][] peopleMap = {{"Mike", "Web Designer"}, {"Jill", "Support"}, {"Jane", "Accountant"}, {"John", "Software Engineer"},
+                {"Sarah", "Product Manager"}, {"Carlos", "Data Analyst"}, {"Emily", "UX Designer"}, {"David", "Project Manager"},
+                {"Maria", "QA Engineer"}, {"Alex", "DevOps Engineer"}};
+        List<WebElement> people = driver.findElements(By.className("w3-padding-16"));
+        assertEquals(10, people.size());
+        for (int i=0; i<peopleMap[2].length; i++) {
+            assertEquals(peopleMap[i][0], people.get(i).findElement(By.className("name")).getText());
+            assertEquals(peopleMap[i][1], people.get(i).findElement(By.className("job")).getText());
+            i++;
+        }
+    }
 }
--- src/test/resources/features/Task2.feature
+++ src/test/resources/features/Task2.feature
@@ -5,3 +5,44 @@
 # - edit a person
 # - remove a person
 # - reset original list after adding a person, editing a person or removing a person
+
+Feature: List of people with jobs page
+  As a user
+  I want to edit the list of people in the page
+
+  Background:
+    Given I am on the list of people with jobs page
+
+  Scenario: Add a new person
+    When I click add person
+    And I fill fields
+      | name | Jusus   |
+      | job  | Savior  |
+    And I click Add button
+    Then I see person added to the list
+      | name | Jusus   |
+      | job  | Savior  |
+
+
+  Scenario: Edit a person
+    When I click Edit button for person: "John"
+    And I see person values
+      | name | John              |
+      | job  | Software Engineer |
+    And I change Job: "Gas Station Casher"
+    And I click Edit button
+    Then I see person updated
+      | name | John               |
+      | job  | Gas Station Casher |
+
+
+  Scenario: Remove a person
+    When I click Delete button for person: "John"
+    Then I see person "John" is removed from the list
+
+
+  Scenario: reset original list after removing a person
+    When I click Delete button for person: "Emily"
+    And I see person "Emily" is removed from the list
+    And click Reset List
+    Then I see list is back to initial state
